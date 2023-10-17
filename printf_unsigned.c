a87297bef8fe622fa491f8254f0bd8c09b9ec782
Abiybeshah <abiyjem99@gmail.com>
2023-10-17 18:33:45 +0300

    those are the remaining functions.

---
+++ printf_unsigned.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* print_unsigned - print integer
+* @args: argument to print
+* Return: number of chars printed
+*/
+
+int print_unsigned(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int num, last = n % 10, dig, exp = 1;
+	int i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = -num;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			dig = num / exp;
+			_putchar(dig + '0');
+			num = num - (dig * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
